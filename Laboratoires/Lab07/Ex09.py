b24ca821aaffe4899f652b43a858cb60a0c24075
JF Monette <jfmonette@gmail.com>
2014-03-04 14:24:27 -0500

    Ajout d'un exercice sur les modules.

--- Laboratoires/Lab07/Ex09.py
+++ Laboratoires/Lab07/Ex09.py
@@ -10,6 +10,7 @@ Revoyons maintenant ce que nous connaissons des fonctions.
 En utilisant des fonctions et ce que vous avez appris jusqu'à maintenant, créez le code vous permettant d'imprimer
 les informations suivantes : 
 
+-La voiture la moins dispendieuse se trouvant dans la variables catalogue du module Honda.
 -La voiture la plus dispendieuse se trouvant dans la variables catalogue du module Honda.
 -La moyenne du prix de vente des voitures.
 
---
+++ Laboratoires/Lab07/Ex10.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014-03-04
+
+@author: moe
+'''
+'''
+Revoyons maintenant ce que nous connaissons des modules et des fonctions.
+
+Renommez ce module pour qu'il se nomme Concessionnaire. Vous devez implémenter des fonctions qui permettront de :
+-Lister l'ensemble des voitures disponibles dans le catalogue Honda.
+-Montrer les informations de la voiture la moins dispendieuse. (pensez à réutiliser vos fonctions du dernier exercice)
+-Montrer les informations de la voiture la moins dispendieuse.
+-Gérer le solde en banque du concessionnaire, c'est à dire d'effectuer des dépôts et des retraits et d'afficher le solde.
+-Créez un module client qui contient une liste de clients fictifs ayant des soldes bancaires.
+-Simuler la vente d'une voiture à un client, c'est à dire :
+  -Déduire le coût de la voiture du solde du client
+  -Ajouter le coût de la voiture au solde du concessionnaire
+  -Ajouter, dans le dictionnaire correspondant au client, une nouvelle entrée afin de représenter sa nouvelle acquisition.
+'''
